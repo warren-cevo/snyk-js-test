0116ded8d57cff7888972613532e9fc79f0e0487
WarrenCevo <warren.abundo@cevo.com.au>
2023-04-27 11:28:27 +1000

    Another vul

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 const fastify = require('fastify')({ logger: true })
 const mysql = require('mysql2');
 
+const password = 'password';
+const secret = "secret";
+
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'snyk-test',
-    password: 'password'
+    password
 });
+
+const AWS_ACCESS_KEY_ID="AKIAJWY3D3GJ6N4UTEST"
+const AWS_SECRET_ACCESS_KEY="CmeRfzYcE2lD1z8AeKkM1E4j9G4fW8o5H2xLTEST"
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-    const results = await connection.promise().query(
-        'SELECT * FROM `users` WHERE id=' + request.query.userId
-    );
+    const results = await connection
+        .promise()
+        .query('SELECT * FROM `users` WHERE id=' + request.query.userId);
 
     return { results: results[0] }
 })
